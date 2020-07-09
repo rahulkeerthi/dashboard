03c447ad1d683e9cf21acee99c1b7c3caaa7096e
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-07-09 23:20:01 +0100

    Added React Router, Material Table, Sweet Alert and set up Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// React
 import React, { Component } from 'react';
-// Spinners
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // Material UI Components
 import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
+import './components/AppBar.scss';
+
+// Other 3rd Party
+import Swal from 'sweetalert2';
+
+// Spinners
 import TineyerSpinner from './components/spinner/TineyerSpinner';
 import TineySpinner from './components/spinner/TineySpinner';
+
+const axios = require('axios').default;
 
 class App extends Component {
   state = {
@@ -16,20 +26,63 @@ class App extends Component {
     error: null,
   };
 
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    const baseUrl = `http://localhost:3001`;
+
+    const providerResponse = await axios.get(`${baseUrl}/providers`).catch((err) => {
+      Swal.fire({
+        title: 'Error!',
+        text: err.message,
+        icon: 'error',
+        confirmButtonText: "I'll try again later",
+      });
+    });
+
+    const childrenResponse = await axios.get(`${baseUrl}/children`).catch((err) => {
+      Swal.fire({
+        title: 'Error!',
+        text: err.message,
+        icon: 'error',
+        confirmButtonText: "I'll try again later",
+      });
+    });
+
+    this.setState({
+      providers: providerResponse.data,
+      children: childrenResponse.data,
+      isLoading: false,
+    });
+  }
+
   render() {
     const { isLoading, providers, children, error } = this.state;
 
     return (
-      <div className="App">
-        <AppBar position="static" className="appbar">
-          {isLoading && <TineySpinner message="Loading..." />}
-          <Tabs>
-            <TineyerSpinner className="appbar__logo" />
-            <Tab label="Providers" className="appbar__tab" />
-            <Tab label="Children" className="appbar__tab" />
-          </Tabs>
-        </AppBar>
-      </div>
+      <Router>
+        <div className="App">
+          <AppBar position="static" className="appbar">
+            {isLoading && <TineySpinner message="Loading..." />}
+            <Tabs>
+              <TineyerSpinner className="appbar__logo" />
+              <Link to="/providers" className="appbar__link">
+                <Tab label="Providers" className="appbar__tab" />
+              </Link>
+              <Link to="/children" className="appbar__link">
+                <Tab label="Children" className="appbar__tab" />
+              </Link>
+            </Tabs>
+          </AppBar>
+          <Container style={{ marginTop: '16px' }}>
+            {error && <TineySpinner message={error} />}
+            <Switch>
+              <Route path={'/providers' || '/'} />
+              <Route exact path="/children" />
+            </Switch>
+          </Container>
+        </div>
+      </Router>
     );
   }
 }
